440a17e1c46a53e856552274d9588bc1d2e72bab
d3monw1ngs <tezzielumsit87@gmail.com>
2024-04-06 19:45:27 +0800

    update files

--- script.js
+++ script.js
@@ -4,6 +4,20 @@ const API_URL = BASE_URL + '/discover/movie?sort_by-popularity.desc&' + API_KEY;
 const IMG_URL = 'https://image.tmdb.org/t/p/w500';
 const searchURL = BASE_URL + '/search/movie?'+API_KEY;
 
+const options = {
+    params: {
+        key: API_KEY,
+        query: "",
+        include_adult: false,
+        language: "en-US",
+        primary_release_year: "",
+        page: 1,
+        region: "",
+        year: "",
+       
+    }
+}
+
 const genre = [
     {
       "id": 28,
@@ -88,6 +102,18 @@ const form = document.getElementById('form');
 const search = document.getElementById('search');
 const tagsEl = document.getElementById('tags');
 
+// PAGINATION
+const prev = document.getElementById('prev');
+const next = document.getElementById('next');
+const current = document.getElementById('current')
+
+var currentPage = 1;
+var nextPage = 2;
+var prevPage = 3;
+var lastUrl = '';
+var totalPages = 100;
+
+
 // GENRE SECTION
 var selectedGenre = []
 setGenre();
@@ -158,17 +184,35 @@ function clearBtn() {
 
 getMovies(API_URL);
 
+// to display movie cards
 function getMovies(url) {
-    
+    lastUrl = url;    
     fetch(url).then(res => res.json()).then(data => {
-       console.log(data.results);
+       console.log(data.results)
        if(data.results.length !== 0){
-        showMovies(data.results);
+            showMovies(data.results);
+            currentPage = data.page;
+            nextPage = currentPage + 1;
+            prevPage = currentPage - 1;
+            totalPages = data.total_pages;
+
+            current.innerText = currentPage;
+            if(currentPage <= 1){
+                prev.classList.add('disabled');
+                next.classList.remove('disabled');
+            }else if(currentPage >= totalPages){
+                prev.classList.remove('disabled');
+                next.classList.add('disabled');
+            }else{
+                prev.classList.remove('disabled');
+                next.classList.remove('disabled');
+            }
+
+            tagsEl.scrollIntoView({behavior : 'smooth'});
+
        }else {
             main.innerHTML = `<h1 class="no-results">No Results Found</h1>`
-
-       }
-        
+       }        
     })
 }
 
@@ -222,3 +266,34 @@ form.addEventListener('submit', (e) => {
         getMovies(API_URL);
     }
 })
+
+//function for previous page
+prev.addEventListener('click', () => {
+    if(prevPage > 0){
+        pageCall(prevPage);
+    }
+})
+
+// function for the next page 
+next.addEventListener('click', () => {
+    if(nextPage <= totalPages){
+        pageCall(nextPage);
+    }
+})
+
+function pageCall(page) {
+    let urlSplit = lastUrl.split('?');
+    let queryParams = urlSplit[1].split('&');
+    let key = queryParams[queryParams.length - 1].split('=');
+    if(key[0] != 'page') {
+        let url = lastUrl + '&page=' + page;
+        getMovies(url);
+    }else{
+        key[1] = page.toString();
+        let a = key.join('=');
+        queryParams[queryParams.length - 1] = a;
+        let b = queryParams.join('&');
+        let url = urlSplit[0] +'?'+ b;
+        getMovies(url);
+    }
+}
