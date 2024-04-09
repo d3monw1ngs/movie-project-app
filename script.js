e015ac83909c0fda4fb2a807fe2e74df60985ac3
d3monw1ngs <tezzielumsit87@gmail.com>
2024-04-09 20:58:49 +0800

    update files

--- script.js
+++ script.js
@@ -17,84 +17,14 @@ const options = {
     }
 }
 
-const genre = [
-    {
-      "id": 28,
-      "name": "Action"
-    },
-    {
-      "id": 12,
-      "name": "Adventure"
-    },
-    {
-      "id": 16,
-      "name": "Animation"
-    },
-    {
-      "id": 35,
-      "name": "Comedy"
-    },
-    {
-      "id": 80,
-      "name": "Crime"
-    },
-    {
-      "id": 99,
-      "name": "Documentary"
-    },
-    {
-      "id": 18,
-      "name": "Drama"
-    },
-    {
-      "id": 10751,
-      "name": "Family"
-    },
-    {
-      "id": 14,
-      "name": "Fantasy"
-    },
-    {
-      "id": 36,
-      "name": "History"
-    },
-    {
-      "id": 27,
-      "name": "Horror"
-    },
-    {
-      "id": 10402,
-      "name": "Music"
-    },
-    {
-      "id": 9648,
-      "name": "Mystery"
-    },
-    {
-      "id": 10749,
-      "name": "Romance"
-    },
-    {
-      "id": 878,
-      "name": "Science Fiction"
-    },
-    {
-      "id": 10770,
-      "name": "TV Movie"
-    },
-    {
-      "id": 53,
-      "name": "Thriller"
-    },
-    {
-      "id": 10752,
-      "name": "War"
-    },
-    {
-      "id": 37,
-      "name": "Western"
-    }
-  ]
+async function fetchGenres() {
+  const res = await fetch(
+    `https://api.themoviedb.org/3/genre/movie/list?${API_KEY}`
+  );
+  const data = await res.json();
+  return data.genres;
+  
+}
 
   // MODAL SECTION
   const modal = document.getElementById('myModal');
@@ -113,7 +43,7 @@ const genre = [
   function openModal(movie) {
       modalPoster.src = `https://image.tmdb.org/t/p/w500/${movie.poster_path}`;
       modalTitle.textContent = movie.title;
-      modalVote.textContent = movie.vote_average+'/'+movie.vote_count;
+      modalVote.textContent = movie.vote_average+' / '+movie.vote_count;
       modalPopularity.textContent = movie.popularity;
       modalOrigTitle.textContent = movie.original_title;
       modalGenre.textContent = movie.genre;
@@ -155,9 +85,10 @@ const search = document.getElementById('search');
 const tagsEl = document.getElementById('tags');
 
 // PAGINATION
+
 const prev = document.getElementById('prev');
 const next = document.getElementById('next');
-const current = document.getElementById('current')
+const current = document.getElementById('current');
 
 var currentPage = 1;
 var nextPage = 2;
@@ -233,8 +164,8 @@ var totalPages = 100;
 //     }
 // }
 
-
 getMovies(API_URL);
+
 
 // to display movie cards
 function getMovies(url) {
@@ -269,29 +200,126 @@ function getMovies(url) {
     })
 }
 
-function showMovies(data) {
-    main.innerHTML = '';
+// declaration of genre list
+const genreList = [
+  {
+    "id": 28,
+    "name": "Action"
+  },
+  {
+    "id": 12,
+    "name": "Adventure"
+  },
+  {
+    "id": 16,
+    "name": "Animation"
+  },
+  {
+    "id": 35,
+    "name": "Comedy"
+  },
+  {
+    "id": 80,
+    "name": "Crime"
+  },
+  {
+    "id": 99,
+    "name": "Documentary"
+  },
+  {
+    "id": 18,
+    "name": "Drama"
+  },
+  {
+    "id": 10751,
+    "name": "Family"
+  },
+  {
+    "id": 14,
+    "name": "Fantasy"
+  },
+  {
+    "id": 36,
+    "name": "History"
+  },
+  {
+    "id": 27,
+    "name": "Horror"
+  },
+  {
+    "id": 10402,
+    "name": "Music"
+  },
+  {
+    "id": 9648,
+    "name": "Mystery"
+  },
+  {
+    "id": 10749,
+    "name": "Romance"
+  },
+  {
+    "id": 878,
+    "name": "Science Fiction"
+  },
+  {
+    "id": 10770,
+    "name": "TV Movie"
+  },
+  {
+    "id": 53,
+    "name": "Thriller"
+  },
+  {
+    "id": 10752,
+    "name": "War"
+  },
+  {
+    "id": 37,
+    "name": "Western"
+  }
+]
 
-    data.forEach(movie => {
-        const {title, poster_path, vote_average, overview} = movie;
-        const movieEl = document.createElement('div');
-        movieEl.classList.add('movie');
-        movieEl.innerHTML = `
-            <img class="poster" src="${poster_path? IMG_URL+poster_path: "http://via.placeholder.com/1080x1500"}" 
-            alt="${title}">
-
-            <div class="movie-info">
-                <h3>${title}</h3>
-                <p></p>
-                
-                <span class="green">${vote_average}</span>
-            </div>
-
-        `
-        movieEl.addEventListener('click', function() {openModal(movie)});
-        main.appendChild(movieEl);
-    })
+// defining the getGenres function
+async function getGenres(genre_ids) {
+  try {
+    let allGenres = await fetchGenres();
+    let collectGenres = allGenres
+      .filter(genre => genre_ids.includes(genre.id))
+      .map(genre => genre.name);
+    return collectGenres;
+  } catch (e) {
+    console.log(e);
+  }
 }
+
+// function to call the movies in the data
+function showMovies(data) {
+  main.innerHTML = '';
+
+  data.forEach(movie => {
+      const { title, poster_path, release_date, vote_average } = movie;
+      const movieEl = document.createElement('div');
+      let year = release_date.split('-');
+      movieEl.classList.add('movie');
+      movieEl.innerHTML = `
+          <img src="${poster_path? IMG_URL+poster_path: "http:/>/via.placeholder.com/1080x1500"}"
+              alt="${title}"/>
+          
+          <div class="movie-info">
+              <h3>${title}</h3>
+              <div class="movie-details">
+                <p id="release_date" class="${release_date}">${release_date}</p>
+                <span id="vote_average" class="${vote_average}">${vote_average}</span>
+                
+              </div>
+          </div>        
+      `
+      movieEl.addEventListener('click', function() { openModal(movie) });
+      main.appendChild(movieEl);
+  })
+}
+
 
 
 
@@ -309,8 +337,8 @@ form.addEventListener('submit', (e) => {
     e.preventDefault();
 
     const searchTerm = search.value;
-    selectedGenre=[];
-    setGenre();
+    // selectedGenre=[];
+    // setGenre();
 
     if(searchTerm) {
         getMovies(searchURL+'&query='+searchTerm)
